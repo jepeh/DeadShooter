8319138f477e43b96cef4534013af7e3d1bcbafc
jepeh <ganzanjepeh01@gmail.com>
2022-01-17 15:55:19 +0800

    .

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import { GAME } from './app/script.js'
 import { Profile } from './profiles/profile.js'
 import * as Utils from './app/utils.js'
 import rewards from './app/rewards.js'
-import * as Three from '../src/three.js'
 
 //GAME.startGame(Profile.level)
 
