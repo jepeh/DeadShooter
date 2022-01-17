b2f5f38b16022ca9c1ae231abf204e0ba2abfbf5
jepeh <ganzanjepeh01@gmail.com>
2022-01-17 16:48:54 +0800

    .

--- app/audio.js
+++ app/audio.js
@@ -1,22 +1,12 @@
-var bomb = new Audio('../assets/audio/bomb.mp3')
-bomb.crossOrigin = "anonymous"
-var eat = new Audio('../assets/audio/eat.mp3')
-eat.crossOrigin = "anonymous"
-var setting = new Audio('../assets/audio/menu.wav')
-setting.crossOrigin = "anonymous"
-var color = new Audio('../assets/audio/color.wav')
-color.crossOrigin = "anonymous"
-var toggle = new Audio('../assets/audio/toggle.wav')
-toggle.crossOrigin = "anonymous"
-var energy = new Audio('../assets/audio/energy.wav')
-energy.crossOrigin = "anonymous"
-var coin = new Audio('../assets/audio/coin.wav')
-coin.crossOrigin = "anonymous"
-var gameWin = new Audio('../assets/audio/gameWin.wav')
-gameWin.crossOrigin = "anonymous"
-var gameOver = new Audio('../assets/audio/gameOver.wav')
-gameOver.crossOrigin = "anonymous"
-
+var bomb = new Audio('assets/audio/bomb.mp3')
+var eat = new Audio('assets/audio/eat.mp3')
+var setting = new Audio('assets/audio/menu.wav')
+var color = new Audio('assets/audio/color.wav')
+var toggle = new Audio('assets/audio/toggle.wav')
+var energy = new Audio('assets/audio/energy.wav')
+var coin = new Audio('assets/audio/coin.wav')
+var gameWin = new Audio('assets/audio/gameWin.wav')
+var gameOver = new Audio('assets/audio/gameOver.wav')
 
 export {gameWin, gameOver, bomb, eat, setting, color, toggle, energy, coin}
 
