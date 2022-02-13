a197dbdcf0e57892334d6775a0a2ec75a64e4b4e
jepeh <ganzanjepeh01@gmail.com>
2022-02-13 11:43:13 +0800

    .

--- App.js
+++ App.js
@@ -1,8 +1,47 @@
+import * as sound from '/app/audio.js'
+
 if ("serviceWorker" in navigator) {
-	window.addEventListener("load", function() {
-		navigator.serviceWorker
-			.register("serviceWorker.js")
-			.then(res => {})
-			.catch(err => console.log("service worker not registered", err))
-	})
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then((res) => {
+        console.log("installed");
+        
+      })
+      .catch((err) => alert(err));
+  });
 }
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  deferredPrompt = e;
+});
+
+var install = document.getElementById("install")
+
+install.addEventListener("click", async () => {
+
+  if (deferredPrompt === null) {
+    
+  } else {
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    if (outcome === "accepted") {
+      deferredPrompt = null;
+    }
+  }
+});
+
+var c = 0;
+
+document.documentElement.addEventListener("click", e => {
+c++
+  if (c > 1) {
+  } else {
+	sound.mainMusic.currentTime > 0 ? sound.mainMusic.currentTime = 0 : false
+	var ss = sound.mainMusic.play()
+	if (ss !== undefined) ss.then(() => {})
+		.catch((e) => { console.warn(e) })
+  }  
+});
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,28 +1,31 @@
-self.addEventListener("install", installEvent => {
-	
-	installEvent.waitUntil(
-		caches.open("static").then(cache => {
-			return cache.addAll(['/', './', 'assets', './assets', '/assets', 'assets/images/blockgameswhite.png'])
-		})
-	);
-})
+self.addEventListener("install", (installEvent) => {
 
-window.location.reload(true)
+  installEvent.waitUntil(
+    caches.open("static").then((cache) => {
+  
+      return cache.addAll([
+        "/",
+        "./",
+        "assets",
+        "./assets",
+        "/assets",
+        "assets/images/blockgameswhite.png",
+      ]);
+    })
+  );
+});
 
+self.addEventListener("fetch", (e) => {
+  console.log(`ferching for ${e.request.url}`);
 
-self.addEventListener("fetch", e =>{
-	console.log(`ferching for ${e.request.url}`)
-})
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-	// Prevent the mini-infobar from appearing on mobile
-	e.preventDefault();
-	// Stash the event so it can be triggered later.
-	deferredPrompt = e;
-	// Update UI notify the user they can install the PWA
-	showInstallPromotion();
-	// Optionally, send analytics event that PWA install promo was shown.
-	console.log(`'beforeinstallprompt' event was fired.`);
+   e.respondWith(
+    caches.match(e.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(e.request);
+      }
+    ));
 });
